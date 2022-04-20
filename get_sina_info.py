06ec11efb7e0e413201c18977c414bc7141091e1
“王宇峰” <wangyufeng@souche.com>
2022-04-20 16:36:35 +0800

    @指定人,+多博主信息

--- get_sina_info.py
+++ get_sina_info.py
@@ -21,15 +21,21 @@ uid_list = [
     userenum["披着羊毛的魔鬼"],
 ]
 base_url = "https://weibo.com/ajax/statuses/mymblog?uid={}&page=1&feature=0"
-keywords = [] #["瑞幸"] 关键字
+keywords = {
+    #"19157707002": ["洗衣机","电视机","电冰箱"],
+    #"13210745239": ["卫生巾"],
+    #"17751793119": ["童鞋"],
+    "17816874619": ["显卡"],
+}
+#["瑞幸"] 关键字
 header = {
     "cookie": "XSRF-TOKEN=Yvum9imXlVvXblTDwHt0EzYL; SUB=_2AkMVAgQ7dcPxrAVQm_ASzW7jaY9H-jym123NAn7uJhMyAxhu7msjqSVutBF-XAFfxzN3IbKWzoaInffUay8gZBa8;",
 }
 
 
-def send_message(message,isAtAll):
-    token = "https://oapi.dingtalk.com/robot/send?access_token=a56088b1a17f1378d808f365a7d91c81414b9abc9722a7605c3f18f3c0b11e8a"
-
+def send_message(message,atMobiles):
+    #token1 = "https://oapi.dingtalk.com/robot/send?access_token=a56088b1a17f1378d808f365a7d91c81414b9abc9722a7605c3f18f3c0b11e8a"
+    token2 = "https://oapi.dingtalk.com/robot/send?access_token=614186589451c0dc0a4583168f23cfc0e9da9768039b77eff8dfc4350c88d00c"
     # 发送Markdown类型的机器人消息
     """发送Markdown类型的机器人消息
         :param message:发送的消息
@@ -45,12 +51,14 @@ def send_message(message,isAtAll):
             "text": message
         },
         "at": {
-
-            "isAtAll": isAtAll,
+            #"atUserIds":atUserIds
+            "atMobiles":atMobiles,
+            #"isAtAll": isAtAll,
         }
     }
-    res = requests.post(token, json=param)
-    print(res)
+
+    #requests.post(token1, json=param)
+    requests.post(token2, json=param)
 
 
 def parse_message(message):
@@ -65,34 +73,41 @@ def parse_message(message):
 
 def get_sheep():
     """获得羊毛信息并发钉钉通知"""
+    phone_list = []
     for uid in uid_list:
         url = base_url.format(uid)
-        data = requests.get(url, headers=header).text
-        if data.startswith("{"):
-            data = json.loads(data)
-        else:
-            print(data)
-            message = "获取更新请求失败了,建议查看下原因"
-            send_message(message,True)
-            raise RuntimeError("获取失败")
-        data_list = data.get("data").get("list")
+        res = requests.get(url, headers=header).json()
+        if res.get("ok") == None and res["ok"] != 1:
+            break
+        data_list = res.get("data").get("list")
         # 判断博主的置顶微博,不是最新的,放在循环外
-        is_top = data_list[0]["isTop"]
-        if (is_top is not None) or is_top == 1:
-            del data_list[0]
+        if len(data_list)!=0 and data_list != []:
+            if data_list[0].get("isTop") != None:
+                is_top = data_list[0].get("isTop")
+                if (is_top is not None) or is_top == 1:
+                    del data_list[0]
+        else:
+            break
         # 比较返回中最新的一条 跟上次请求保存在本地最大的一条
         id_json = read_json()
         if data_list[0]["id"] != id_json[uid]:
             for item in data_list:
                 #依次从大到小，跟保存本地的那条数据做校验
                 if  item["id"] > id_json[uid]:
-                    message = parse_message(item.get("user").get("screen_name")+'\\\n'+'\\\n'+item.get("text"))
+                    message = parse_message(item.get("user").get("screen_name") + '\\\n' + '\\\n' + item.get("text"))
+                    if item["pic_num"] == 1:
+                        pic = item["pic_infos"][item["pic_ids"][0]]["bmiddle"]["url"]
+                        message = parse_message(item.get("user").get("screen_name")+'\\\n'+'\\\n'+item.get("text")+'\\\n'+"![]"+"("+pic+")")
                     if len(keywords) == 0:
-                        send_message(message,False)
-                    for word in keywords:
+                        send_message(message,None)
+                    for key in keywords:
                         # 如果关键字在博主发的消息和链接的文字中含有,就发送请求
-                        if word in str(str(message)+str(item["url_struct"][0]["url_title"])):
-                            send_message(message,True)
+                        keylist = keywords[key]
+                        for word in keylist:
+                            if word in str(str(message)+str(item["url_struct"][0]["url_title"])):
+                                phone_list.append(key)
+                                message = message + "@"+key
+                                send_message(message,phone_list)
                 if item["id"] == id_json[uid]:
                     break
         # 获取到本次请求最大的id，最新的一条存到本地
@@ -106,6 +121,6 @@ def dojob():
     #创建调度器：BlockingScheduler
     scheduler = BlockingScheduler()
     #添加任务,时间间隔2S
-    scheduler.add_job(get_sheep, 'interval', seconds=7,max_instances=5)
+    scheduler.add_job(get_sheep, 'interval', seconds=6,max_instances=5)
     scheduler.start()
 dojob()
