c673903bde301b7323342c64079880f16d32a7f7
“王宇峰” <wangyufeng@souche.com>
2022-04-22 10:54:50 +0800

    fix bug

--- get_sina_info.py
+++ get_sina_info.py
@@ -9,23 +9,23 @@ import re
 
 userenum = {
     "薅羊毛的大队长": "5069029750",
-    #"蘑菇小牙牙":"1987241375",
-    #"小屁屁挖白菜":"3194506490",
+    "蘑菇小牙牙":"1987241375",
+    "小屁屁挖白菜":"3194506490",
     "披着羊毛的魔鬼":"1917872472",
 }
 
 uid_list = [
     userenum["薅羊毛的大队长"],
-    #userenum["蘑菇小牙牙"],
-    #userenum["小屁屁挖白菜"],
+    userenum["蘑菇小牙牙"],
+    userenum["小屁屁挖白菜"],
     userenum["披着羊毛的魔鬼"],
 ]
 base_url = "https://weibo.com/ajax/statuses/mymblog?uid={}&page=1&feature=0"
 keywords = {
     #"19157707002": ["洗衣机","电视机","电冰箱"],
-    #"13210745239": ["卫生巾"],
-    #"17751793119": ["童鞋"],
-    "17816874619": ["显卡"],
+    "13210745239": ["卫生巾"],
+    "17751793119": ["牛肉干"],
+    #"17816874619": ["显卡"],
 }
 #["瑞幸"] 关键字
 header = {
@@ -34,8 +34,10 @@ header = {
 
 
 def send_message(message,atMobiles):
-    #token1 = "https://oapi.dingtalk.com/robot/send?access_token=a56088b1a17f1378d808f365a7d91c81414b9abc9722a7605c3f18f3c0b11e8a"
-    token2 = "https://oapi.dingtalk.com/robot/send?access_token=614186589451c0dc0a4583168f23cfc0e9da9768039b77eff8dfc4350c88d00c"
+    #我公司的群
+    token1 = "https://oapi.dingtalk.com/robot/send?access_token=a56088b1a17f1378d808f365a7d91c81414b9abc9722a7605c3f18f3c0b11e8a"
+    #洛哥的群
+    #token2 = "https://oapi.dingtalk.com/robot/send?access_token=614186589451c0dc0a4583168f23cfc0e9da9768039b77eff8dfc4350c88d00c"
     # 发送Markdown类型的机器人消息
     """发送Markdown类型的机器人消息
         :param message:发送的消息
@@ -57,8 +59,8 @@ def send_message(message,atMobiles):
         }
     }
 
-    #requests.post(token1, json=param)
-    requests.post(token2, json=param)
+    requests.post(token1, json=param)
+    #requests.post(token2, json=param)
 
 
 def parse_message(message):
@@ -104,10 +106,12 @@ def get_sheep():
                         # 如果关键字在博主发的消息和链接的文字中含有,就发送请求
                         keylist = keywords[key]
                         for word in keylist:
-                            if word in str(str(message)+str(item["url_struct"][0]["url_title"])):
-                                phone_list.append(key)
-                                message = message + "@"+key
-                                send_message(message,phone_list)
+                            if item.get("url_struct") != None:
+                                if word in str(str(message)+str(item["url_struct"][0]["url_title"])):
+                                    phone_list.append(key)
+                                    message = message + "@"+key
+                                    send_message(message,phone_list)
+                    send_message(message,None)
                 if item["id"] == id_json[uid]:
                     break
         # 获取到本次请求最大的id，最新的一条存到本地
@@ -121,6 +125,6 @@ def dojob():
     #创建调度器：BlockingScheduler
     scheduler = BlockingScheduler()
     #添加任务,时间间隔2S
-    scheduler.add_job(get_sheep, 'interval', seconds=6,max_instances=5)
+    scheduler.add_job(get_sheep, 'interval', seconds=8,max_instances=5)
     scheduler.start()
 dojob()
