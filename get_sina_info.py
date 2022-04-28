1bdfe7997bd62a060c84b43c73dbdd00b7e12c61
“王宇峰” <wangyufeng@souche.com>
2022-04-28 17:25:20 +0800

    fix bug

--- get_sina_info.py
+++ get_sina_info.py
@@ -21,13 +21,15 @@ uid_list = [
     userenum["披着羊毛的魔鬼"],
 ]
 base_url = "https://weibo.com/ajax/statuses/mymblog?uid={}&page=1&feature=0"
-keywords = {
+# keywords = {
     #"19157707002": ["洗衣机","电视机","电冰箱"],
     #"13210745239": ["卫生巾"],#凯乐
-    "17751793119": ["牛肉干"], #淑环
+    # "17751793119": ["餐具"], #淑环
     #"17816874619": ["显卡"], #洛哥
     #"15757176965":["牛奶"] #严鸽
-}
+    # "17816874847":["纸巾"] #凌峰
+# }
+keywords = read_json("keywords.json")
 #["瑞幸"] 关键字
 header = {
     "cookie": "XSRF-TOKEN=Yvum9imXlVvXblTDwHt0EzYL; SUB=_2AkMVAgQ7dcPxrAVQm_ASzW7jaY9H-jym123NAn7uJhMyAxhu7msjqSVutBF-XAFfxzN3IbKWzoaInffUay8gZBa8;",
@@ -38,7 +40,7 @@ def send_message(message,atMobiles):
     #我公司的群
     token1 = "https://oapi.dingtalk.com/robot/send?access_token=a56088b1a17f1378d808f365a7d91c81414b9abc9722a7605c3f18f3c0b11e8a"
     #洛哥的群
-    #token2 = "https://oapi.dingtalk.com/robot/send?access_token=614186589451c0dc0a4583168f23cfc0e9da9768039b77eff8dfc4350c88d00c"
+    token2 = "https://oapi.dingtalk.com/robot/send?access_token=614186589451c0dc0a4583168f23cfc0e9da9768039b77eff8dfc4350c88d00c"
     # 发送Markdown类型的机器人消息
     """发送Markdown类型的机器人消息
         :param message:发送的消息
@@ -61,7 +63,7 @@ def send_message(message,atMobiles):
     }
 
     requests.post(token1, json=param)
-    #requests.post(token2, json=param)
+    requests.post(token2, json=param)
 
 
 def parse_message(message):
@@ -80,7 +82,7 @@ def get_sheep():
         url = base_url.format(uid)
         res = requests.get(url, headers=header).json()
         if res.get("ok") == None and res["ok"] != 1:
-            break
+            continue
         data_list = res.get("data").get("list")
         # 判断博主的置顶微博,不是最新的,放在循环外
         if len(data_list)!=0 and data_list != []:
@@ -89,9 +91,9 @@ def get_sheep():
                 if (is_top is not None) or is_top == 1:
                     del data_list[0]
         else:
-            break
+            continue
         # 比较返回中最新的一条 跟上次请求保存在本地最大的一条
-        id_json = read_json()
+        id_json = read_json("id.json")
         if data_list[0]["id"] != id_json[uid]:
             for item in data_list:
                 #依次从大到小，跟保存本地的那条数据做校验
@@ -100,6 +102,12 @@ def get_sheep():
                     if item["pic_num"] == 1:
                         pic = item["pic_infos"][item["pic_ids"][0]]["bmiddle"]["url"]
                         message = parse_message(item.get("user").get("screen_name")+'\\\n'+'\\\n'+item.get("text")+'\\\n'+"![]"+"("+pic+")")
+                    if item.get("user").get("screen_name") == "薅羊毛的大队长":
+                        phone_list = []
+                        phone_list.append("19157707002")
+                        message = message + " @19157707002"
+                        send_message(message, phone_list)
+                        continue
                     if len(keywords) == 0:
                         send_message(message,None)
                     if len(keywords) != 0:
@@ -122,7 +130,7 @@ def get_sheep():
         # 获取到本次请求最大的id，最新的一条存到本地
             id = data_list[0]["id"]
             id_json[uid] = id
-            write_json(id_json)
+            write_json(id_json,"id.json")
 
 # if __name__ == '__main__':
 #     get_sheep()
--- ymlFile.py
+++ ymlFile.py
@@ -2,11 +2,11 @@ import json
 
 
 #覆盖json文件写入
-def write_json(data):
-    with open('./id.json', 'w', encoding='utf-8') as fp:
+def write_json(data,txt):
+    with open(txt, 'w', encoding='utf-8') as fp:
         json.dump(data, fp, ensure_ascii=False)
 
 #读取json文件
-def read_json():
-    return json.load(open('id.json', 'r', encoding="utf-8"))
+def read_json(txt):
+    return json.load(open(txt, 'r', encoding="utf-8"))
 
